69eaea5098a0b00b403bdd78863f373a2c977f82
Alisa Chang <laptite@gmail.com>
2020-06-28 14:09:43 -0700

    Update server file to handle multiple pages

--- src/server.js
+++ src/server.js
@@ -3,8 +3,14 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
-app.get('/index.html', function(req, res) {
+app.get('/index/', function(req, res) {
   const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+  res.send(contentFromHtmlFile);
+});
+
+app.get('/kiwi/', function(req, res) {
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/kiwi.html');
   const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
   res.send(contentFromHtmlFile);
 });
