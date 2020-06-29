c58eca84cc2748068c2e06574c8282f845af7c9e
Alisa Chang <laptite@gmail.com>
2020-06-29 16:23:43 -0700

    Installs, imports and invokes fontawesome

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import TutorialBtn from './components/tutorial-btn/tutorial-btn';
 import Heading from './components/heading/heading';
+
+library.add(faSpinner);
+dom.watch();
 
 const heading = new Heading();
 heading.render('Tutorial Btn');
